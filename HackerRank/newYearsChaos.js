cd431bee8b95eeb5c053cc02c6c20b2337a8c56b
github username <matthewdizon94@protonmail.com>
2019-03-24 19:47:04 -0400

    newYears done

--- HackerRank/newYearsChaos.js
+++ HackerRank/newYearsChaos.js
@@ -13,9 +13,37 @@
 // minimumBribes has the following parameter(s):
 //
 // q: an array of integers
-//  Print Too chaotic if the state is invalid, i.e. it requires a person to have bribed more than 2 people.
+//  Print Too chaotic if the state is invalid, i.e. it requires a person to have bribed more than 2 people
 
 function minimumBribes(q) {
+    let bribes = 0;
+        let swapped = true;
+        let tooChaotic = false;
+        let firstRun = true;
 
+        while (swapped === true) {
+            swapped = false;
+            for (let i = 0; q.length > i; i++) {
+                if (q[i] - i > 3 && firstRun) {
+                    tooChaotic = true;
+                }
+
+                if (q[i] && q[i + 1] && q[i] > q[i + 1]) {
+                    let temp = q[i];
+                    q[i] = q[i + 1];
+                    q[i + 1] = temp;
+                    swapped = true;
+                    bribes++;
+                }
+            }
+            firstRun = false;
+        }
+
+        if (tooChaotic) {
+            console.log("Too chaotic");
+        } else {
+            console.log(bribes);
+        }
 
 }
+minimumBribes([2,5,1,3,4])
