9879bb64dc3cb07f60f8d454bd665a593a9d309f
github username <matthewdizon94@protonmail.com>
2019-03-24 15:12:44 -0400

    leftRot done

---
+++ HackerRank/leftRotation.js
@@ -0,0 +1,21 @@
+// A left rotation operation on an array shifts each of the array's elements 1  unit to the left. For example, if 2 left rotations are performed on array 1,2,3,4,5 , then the array would become 3,4,5,1,2.
+//
+// Given an array a of n integers and a number,d , perform d left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+//
+// Input:
+//     5 4 =>
+//     1 2 3 4 5
+// Output:
+//     5 1 2 3 4
+
+function rotLeft(a, d) {
+    let truncated = a.splice(0, d)
+
+    for (let i = 0; i <= truncated.length - 1; i++) {
+        a.push(truncated[i])
+    }
+    console.log(a)
+
+}
+
+rotLeft([1,2,3,4,5],4)
