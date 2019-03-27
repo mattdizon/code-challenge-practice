87efeedc607aa10368f9be0e4d34b069820d056d
github username <matthewdizon94@protonmail.com>
2019-03-27 16:56:58 -0400

    added triplecount challenge

--- HackerRank/Hash/anagram.js
+++ HackerRank/Hash/anagram.js
@@ -6,131 +6,26 @@
 //     s: a string .
 // iterate through string and get all possible combinations of sub strings
 // iterate through this array of combinations and see if there is another element
+function sherlockAndAnagrams(s) {
+    let count = 0;
 
-function getPairs(s){
-    pairs = []
-    for(let i = 0; i < s.length; i++){
-        for(let j = i + 1; j <= s.length; j++){
-              pairs.push(s.slice(i, j));
-          }
-    }
-    return pairs
-}
-function anagram(str1, str2){
-  if(str1.length !== str2.length) return false;
-  let lettersIn1 = {};
-  let lettersIn2 = {};
+    // Size of our sliding window
+    for (let i = 1; i < s.length; i++) {
+        let found = {};
 
-  for(let i = 0; i < str1.length; i++){
-    let currLetter1 = str1[i];
-    let currLetter2 = str2[i];
-
-    if(lettersIn1[currLetter1] === undefined){
-      lettersIn1[currLetter1] = 1;
-    }else lettersIn1[currLetter1]++;
-
-    if(lettersIn2[currLetter2] === undefined){
-      lettersIn2[currLetter2] = 1;
-    }else lettersIn2[currLetter2]++;
-  }
-
-  for(let letter in lettersIn1){
-    if(lettersIn1[letter] !== lettersIn2[letter]) return false;
-  }
-
-  return true;
-}
-function sherlockAndAnagrams(s){
-    let count = 0
-    let pairs = getPairs(s)
-    for(let i = 1; i < pairs.length; i++){
-        let current = pairs[i]
-        let j = i+1
-
-        while(j < pairs.length){
-            let next = pairs[j];
-            if(anagram(current, next)){
-                count++;
+        // Starting index of our sliding window
+        for (let j = 0; j + i <= s.length; j++) {
+            let substr = s.substr(j, i);
+            substr = substr.split('').sort().join('');
+            if (found[substr]) {
+                count += found[substr];
+                found[substr]++;
+            } else {
+                found[substr] = 1;
             }
-            j++;
         }
     }
-    return count
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function anagram(str1, str2){
-//   if(str1.length !== str2.length) return false;
-//   let lettersIn1 = {};
-//   let lettersIn2 = {};
-//
-//   for(let i = 0; i < str1.length; i++){
-//     let currLetter1 = str1[i];
-//     let currLetter2 = str2[i];
-//
-//     if(lettersIn1[currLetter1] === undefined){
-//       lettersIn1[currLetter1] = 1;
-//     }else lettersIn1[currLetter1]++;
-//
-//     if(lettersIn2[currLetter2] === undefined){
-//       lettersIn2[currLetter2] = 1;
-//     }else lettersIn2[currLetter2]++;
-//   }
-//
-//   for(let letter in lettersIn1){
-//     if(lettersIn1[letter] !== lettersIn2[letter]) return false;
-//   }
-//
-//   return true;
-// }
-//
-// function pairsMaker(str){
-//   let output = [];
-//
-//   for(let i = 0; i < str.length; i++){
-//     for(let j = i + 1; j <= str.length; j++)
-//       output.push(str.slice(i, j));
-//   }
-//   console.log(output)
-//   return output;
-// }
-//
-// function sherlockAndAnagrams(s){
-//   let anagramCount = 0;
-//   let pairs = pairsMaker(s);
-//
-//   for(let i = 0; i < pairs.length; i++){
-//     let currSubStr = pairs[i];
-//     let j = i + 1;
-//
-//     while(j < pairs.length){
-//       let nextSubStr = pairs[j];
-//       if(anagram(currSubStr, nextSubStr)) anagramCount++;
-//       j++;
-//     }
-//   }
-//
-//   return anagramCount;
-// }
+    return count;
+}w
 console.log(sherlockAndAnagrams("abba"));
---
+++ HackerRank/Hash/countTriple.js
@@ -0,0 +1,14 @@
+// You are given an array and you need to find number of tripets of indices  such that the elements at those indices are in geometric progression for a given common ratio  and .
+
+
+// input:
+    // arr of numbers
+    // the ratio to look for
+// output: count of triplets
+
+// steps
+// read in array take the first element(n), compare it with the n+1 if n+1/n = r add it to the array, then check the n+2 element,if n+2/n+1 = r add it if not check the next
+function countTriplets(arr, r) {
+
+
+}
