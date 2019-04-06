311ed289790f00bcb8698d14369ac3a6a6526624
github username <matthewdizon94@protonmail.com>
2019-04-05 22:23:43 -0400

    added bubbleSort

---
+++ HackerRank/Sort/bubbleSort.js
@@ -0,0 +1,15 @@
+// Function Description
+// Complete the function countSwaps in the editor below. It should print the three lines required, then return.
+//
+// countSwaps has the following parameter(s):
+//     a: an array of integers .
+//
+// Output: print the following
+//     Array is sorted in 3 swaps.
+//     First Element: 1
+//     Last Element: 6
+
+function countSwaps(a) {
+
+
+}
