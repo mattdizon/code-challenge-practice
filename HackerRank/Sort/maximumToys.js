9ddf875fc5475a2c4fa8e1f830136555653a6e64
github username <matthewdizon94@protonmail.com>
2019-04-07 09:39:07 -0400

    almost done

--- HackerRank/Sort/maximumToys.js
+++ HackerRank/Sort/maximumToys.js
@@ -2,9 +2,22 @@
 //
 // maximumToys has the following parameter(s):
 //  prices: an array of integers representing toy prices
+        //prices are not sorted
 //  k: an integer, Mark's budget
 
 function maximumToys(prices, k) {
-
-
+    //worst sort method to use bubblesort(O(n^2))
+    budget = 0
+    toys = 0
+    for(i = 0; i < prices.length; i++){
+        for(j = 0; j < prices.length; j++){
+            if(prices[j]>prices[j+1]){
+                temp = prices[j]
+                prices[j] = prices[j+1]
+                prices[j+1] = temp
+            }
+        }
+    }
+    
 }
+maximumToys([3,1,2,6,5],2)
