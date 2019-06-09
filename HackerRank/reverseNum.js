1d17e6d3493b7be33da1664cac2f468b6a46be24
github username <matthewdizon94@protonmail.com>
2019-06-09 17:57:42 -0400

    completed challenge

--- HackerRank/reverseNum.js
+++ HackerRank/reverseNum.js
@@ -1,3 +1,17 @@
 function rev(num){
-    
+    let x = (num.toString().split(""))
+    let newNum = ""
+    if(x[0] === "-"){
+        newNum+=(x[0])
+        for(let i = x.length - 1; i > 0; i --){
+            newNum += (x[i])
+        }
+    }
+    else{
+        for(let i = x.length - 1; i >= 0; i --){
+            newNum += (x[i])
+        }
+    }
+    console.log(newNum.toString())
 }
+rev(321)
