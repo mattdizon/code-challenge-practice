47c66ca4cff6f73aed67af78c777f5e00d2498f6
github username <matthewdizon94@protonmail.com>
2019-06-09 09:16:57 -0400

    added skyline challenge

---
+++ HackerRank/Arrays/skyline.js
@@ -0,0 +1,6 @@
+// you would like to paint the skyline using continuos horizontal brushstrokes. Every horizontal stroke is one unit high and arbitrarily wide
+// The goal is to calculate the number of min horiz strokes needed
+
+//Input: [1,3,2,1,2,1,5,3,3,4,2]
+//Output: 9
+
