e14a8fd08fa80572d86000ced900819ef54257d2
github username <matthewdizon94@protonmail.com>
2019-06-09 15:38:55 -0400

    finished

--- HackerRank/Arrays/skyline.js
+++ HackerRank/Arrays/skyline.js
@@ -4,3 +4,13 @@
 //Input: [1,3,2,1,2,1,5,3,3,4,2]
 //Output: 9
 
+function solution(A){
+    let result = 0;
+    for (let i=1; i<A.length; i++){
+      result += Math.max(0, A[i-1] - A[i]);
+    }
+  
+    return result + A[A.length-1];
+  }
+  
+
