26e46b2969b0fadab4c1f09600212b19141d103f
github username <matthewdizon94@protonmail.com>
2019-06-09 09:21:15 -0400

    bestTrip added

---
+++ HackerRank/Arrays/bestTrip.js
@@ -0,0 +1,8 @@
+// N cities arranged in a row, numbered 0 - N-1
+// each city has an attractive lvel denoted by an int
+// you would like to visit 2 cities, can visit same city twice
+// write a fnction to find the trip with the highest appeal
+// appeal = A[i] + A[j] + (i -j)
+
+// input: Array 
+// Output highest possible appeal 
